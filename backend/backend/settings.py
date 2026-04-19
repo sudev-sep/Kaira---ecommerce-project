d57b30b23ec009050fe129bd53ba1668bd17e353
Sudev A <sudevanss4@gmail.com>
2026-04-19 23:34:51 +0530

    Enhance DATABASES settings with SSL and max age
    Update DATABASES configuration to include connection options.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,9 +85,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 if os.getenv('DATABASE_URL'):
     DATABASES = {
-        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+        'default': dj_database_url.parse(
+            os.getenv('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
     DATABASES = {
